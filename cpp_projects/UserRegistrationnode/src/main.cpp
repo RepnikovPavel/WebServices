b47015c0fa6ca268d178f994da10d7c28a939c30
Repnikov Pavel <prepnik@mail.ru>
2023-06-08 00:45:34 +0300

    crow working fine

--- cpp_projects/UserRegistrationnode/src/main.cpp
+++ cpp_projects/UserRegistrationnode/src/main.cpp
@@ -1,7 +1,23 @@
-#include <crow/app.h>
+#include <crow.h>
 #include <iostream>
+#include <thread>
+
 int main(){
-    std::cout << "im alive"<< std::endl;
+    std::uint16_t port{8080};
+
     crow::SimpleApp app;
+    app.port(port);
+    CROW_ROUTE(app, "/route1")(
+        [](){return "OK";}
+    );
+    CROW_ROUTE(app, "/route2").\
+    methods(crow::HTTPMethod::POST)([&](const crow::request &req){
+        return crow::response(200, "POST METHOD");
+    });
+    std::thread t([&](){
+        std::cout << "listening on port 8080"<< std::endl;
+        app.run();
+    });
+    t.join();
     return EXIT_SUCCESS;
 }
