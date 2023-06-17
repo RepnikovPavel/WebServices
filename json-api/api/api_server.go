a1219f1c15c5982a61f3c2e34dd53e4640aa2bb5
Repnikov Pavel <prepnik@mail.ru>
2023-06-18 01:26:00 +0300

    add simple api errors, add decorator for payload service function

--- json-api/api/api_server.go
+++ json-api/api/api_server.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func DoSomeStuff() {
@@ -12,5 +17,48 @@ type ServerConfig struct {
 	Sockaddr string
 }
 
+type payload func(w http.ResponseWriter, r *http.Request) error
+
+// TODO: make advanced custom api errors
+const (
+	ApiError0 string = "api error 0"
+	ApiError1 string = "api error 1"
+)
+
+func WriteJSON(w http.ResponseWriter, statusCode int, v any) error {
+	w.WriteHeader(statusCode)
+	// Header signature is
+	// type Header map[string][]string
+	// Set(key: x string,value: y string)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeHTTPHandleFunc(f payload) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			// TODO: make advanced error handling
+			WriteJSON(w, http.StatusBadRequest, ApiError0)
+			log.Println(fmt.Sprintf("error: %s\nin %s", err, "/api_server/makeHTTPHadleFunc"))
+
+		}
+		// TODO: make advanced custom api errors
+	}
+}
+
 type Server struct {
 }
+
+func (s *Server) Run() {
+	router := mux.NewRouter()
+
+	/*
+		takes path string and
+		handle function
+		type HandlerFunc func(ResponseWriter, *Request)
+	*/
+	router.HandleFunc()
+}
+
+// CRUD for accounts
