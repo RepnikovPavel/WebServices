6d8d1be9268c9c2005f4f5e514b2ccbf904da8ec
Repnikov Pavel <prepnik@mail.ru>
2023-06-18 16:18:37 +0300

    api server mvp

--- json-api/api/api_server.go
+++ json-api/api/api_server.go
@@ -2,28 +2,28 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func DoSomeStuff() {
-	fmt.Println("some stuff")
+// api errors segment
+type ApiError struct {
+	err string
 }
 
-type ServerConfig struct {
-	Sockaddr string
+func (ApiErr ApiError) Error() string {
+	return ApiErr.err
 }
 
-type payload func(w http.ResponseWriter, r *http.Request) error
-
-// TODO: make advanced custom api errors
 const (
-	ApiError0 string = "api error 0"
-	ApiError1 string = "api error 1"
+	ApiError0                        string = "api error 0"
+	ApiError1                        string = "api error 1"
+	ApiErrorUnknownHTTPRequestMethod string = "api error UnknownHTTPRequestMethod"
 )
+
+// end of epi errors segment
 
 func WriteJSON(w http.ResponseWriter, statusCode int, v any) error {
 	w.WriteHeader(statusCode)
@@ -34,22 +34,81 @@ func WriteJSON(w http.ResponseWriter, statusCode int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
+type payload func(w http.ResponseWriter, r *http.Request) error
+
 func makeHTTPHandleFunc(f payload) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		err := f(w, r)
-		if err != nil {
+		payload_err := f(w, r)
+		if payload_err != nil {
 			// TODO: make advanced error handling
-			WriteJSON(w, http.StatusBadRequest, ApiError0)
-			log.Println(fmt.Sprintf("error: %s\nin %s", err, "/api_server/makeHTTPHadleFunc"))
-
+			json_err := WriteJSON(w, http.StatusBadRequest, ApiError0)
+			log.Printf("error: %s\nin %s", payload_err, "/api_server/makeHTTPHadleFunc/payload_func")
+			if json_err != nil {
+				log.Printf("error: %s\nin %s", json_err, "/api_server/makeHTTPHadleFunc/payload_function/WriteJSON_func")
+			}
 		}
-		// TODO: make advanced custom api errors
 	}
 }
 
-type Server struct {
+type ServerConfig struct {
+	Sockaddr string
 }
 
+type Server struct {
+	Sockaddr string
+}
+
+func NewServer(conf ServerConfig) *Server {
+	return &Server{
+		Sockaddr: conf.Sockaddr,
+	}
+}
+
+const (
+	rout0 string = "rout0"
+	rout1 string = "rout1"
+	rout2 string = "rout2"
+	rout3 string = "rout3"
+	rout4 string = "rout4"
+)
+
+// CRUD for accounts
+// functions signature is payload function:
+// type payload func(w http.ResponseWriter, r *http.Request) error
+
+func PostAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func DeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func PutAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func GetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func HandleAccount(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case "POST":
+		return PostAccount(w, r)
+	case "DELETE":
+		return DeleteAccount(w, r)
+	case "PUT":
+		return PutAccount(w, r)
+	case "GET":
+		return GetAccount(w, r)
+	default:
+		defer log.Printf("warning: caller side error in %s\ninput HTTP method %s is not supported", "HandleAccount", r.Method)
+		return ApiError{err: ApiErrorUnknownHTTPRequestMethod}
+	}
+}
+
+// blocking function
 func (s *Server) Run() {
 	router := mux.NewRouter()
 
@@ -58,7 +117,13 @@ func (s *Server) Run() {
 		handle function
 		type HandlerFunc func(ResponseWriter, *Request)
 	*/
-	router.HandleFunc()
-}
+	/*ServeHTTP(ResponseWriter, *Request)*/
+	router.HandleFunc(rout0, makeHTTPHandleFunc(HandleAccount))
+	/* ListenAndServe(addr string, handler Handler) error */
+	log.Printf("Listern Server has started on sockaddr=%s", s.Sockaddr)
+	startup_error := http.ListenAndServe(s.Sockaddr, router)
+	if startup_error != nil {
+		log.Printf("error: %s\nin %s", startup_error, "Server/Run/http.ListenAndServe")
+	}
 
-// CRUD for accounts
+}
--- json-api/un1234.go
+++ json-api/un1234.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"json_api_project/api"
 	rendering "json_api_project/cmd_ui"
-	"time"
 )
 
 func main() {
 	conf := api.ServerConfig{
-		Sockaddr: "1234",
+		Sockaddr: ":6666",
 	}
-	fmt.Println(conf.Sockaddr)
-	x := 1
 	go rendering.Spinner()
-	fmt.Print(x)
-	api.DoSomeStuff()
-	time.Sleep(10 * time.Second)
+	server := api.NewServer(conf)
+	server.Run()
 }
